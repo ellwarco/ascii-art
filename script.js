3fa9b1b4d4ce617de5d5245ca70d6fd085cca42a
kurumkan <joomrise@gmail.com>
2018-02-24 02:24:57 +0300

    add feat convert images to ascii

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var canvas = document.getElementById('canvas');
 var input = document.getElementById('file-input');
+var result = document.getElementById('result');
+
+var canvas = document.createElement('canvas')//document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 input.addEventListener('change', function() {
@@ -10,7 +12,10 @@ input.addEventListener('change', function() {
         var img = new Image();
         img.src = reader.result;
         img.addEventListener('load', function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
             ctx.drawImage(img, 0, 0);
+            processImage(img);
         }, false);
     }, false);
 
@@ -18,3 +23,28 @@ input.addEventListener('change', function() {
         reader.readAsDataURL(file);
     }
 });
+
+function processImage(img) {
+    var chars = ['&nbsp;', '\'', '.', ':', '|', 'T',  'X', '0', '#'];
+    var width = img.width;
+    var height = img.height;
+    var markup = '';
+
+    //in here our scale is 3 - it means we count only 1 of 3 pixels
+    for(var y = 0; y < height; y += 3) {
+        for(var x = 0; x < width; x += 3) {
+            // get rgb
+            var pixel = ctx.getImageData(x, y, 1, 1);
+            var data = pixel.data;
+            var r = data[0];
+            var g = data[1];
+            var b = data[2];
+
+            var index = Math.floor((r + g + b) * (chars.length - 1) /(255 * 3));
+            var ch = chars[index];
+            markup += ch;
+        }
+        markup += '<br>';
+    }
+    result.innerHTML = markup;
+}
